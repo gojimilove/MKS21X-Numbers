70664775073508233d02ce00932c31c29502e01d
Jessica Kim <jkim00@stuy.edu>
2018-10-18 00:36:24 -0400

    finished rational and real, updated driver

--- RationalNumber.java
+++ RationalNumber.java
@@ -5,6 +5,10 @@ public class RationalNumber extends RealNumber{
 		super(0);
 		n = num;
 		d = den;
+		if (d < 0) {
+			n = num * -1;
+			d = den * -1;
+		}
 		if (d == 0) {
 			n = den;
 			d = num;
@@ -36,7 +40,7 @@ public class RationalNumber extends RealNumber{
 	public int getDenominator() {
 		return d;
 	}
-/*
+
 	public RationalNumber reciprocal() {
 		RationalNumber newReciprocal = new RationalNumber(d, n);
 		return newReciprocal;
@@ -45,11 +49,36 @@ public class RationalNumber extends RealNumber{
 	public boolean equals(RationalNumber other) {
 		return (n == other.getNumerator() && d == other.getDenominator());
 	}
-*/
+
 	private static int gcd(int a, int b) {
-		return 1;
-		//if (b == 0) return a;
-		//else return gcd(b, b % a);
+		int x = Math.max(Math.abs(a), Math.abs(b));
+		int y = Math.min(Math.abs(a), Math.abs(b));
+		if (y == 0) return x;
+		else {
+			return gcd(y, x % y);
+		}
+
 	}
+
+	public RationalNumber multiply(RationalNumber other){
+    RationalNumber multiplied = new RationalNumber((n * other.getNumerator()), (d * other.getDenominator()));
+    return multiplied;
+  }
+
+
+  public RationalNumber divide(RationalNumber other){
+  	RationalNumber divided = new RationalNumber((n * other.getDenominator()), (d * other.getNumerator()));
+    return divided;
+  }
+  
+  public RationalNumber add(RationalNumber other){
+  	RationalNumber added = new RationalNumber((n * other.getDenominator() + other.getNumerator() * d), (d * other.getDenominator()));
+    return added;
+  }
+  
+  public RationalNumber subtract(RationalNumber other){
+    RationalNumber subtracted = new RationalNumber((n * other.getDenominator() - other.getNumerator() * d), (d * other.getDenominator()));
+    return subtracted;
+  }
 
 }
